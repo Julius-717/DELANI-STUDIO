692f6d5b67693b91b0694314603277e7850faa0f
Julius-717 <waynejulius10@gmail.com>
2019-09-30 08:54:15 +0300

    add a README.md

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# IP3
+# Delani Studio.
+
+## Author : JULIUS KABUI
+
+#### Author`s information
+    Email:waynejulius10@gmail.com
+    software developer.
+    tel number:0792627573
+
+## Description
+This is a delani studio providing a plarform for clients to see the various projects that have been worked on by Delani studio company.  
+
+## Setup Requirements
+* Click on cover page to go to About section
+* Click on images on what we do to view texts
+* Hover or mouseover on images on portfolio to view project details
+* fill in the form on each and every input field
+* Click the submit button at the bottom
+* An alert message will appear if showing success or failure
+
+There is no other code that may be crucial to access this material
+
+## Known Bugs
+    None at the moment
+
+## Technologies Used
+    I used HTML,CSS,JQUERY,JAVASCRIPT,BOOTSTRAP.
+
+## Support and Contact details
+    contact me via waynejulius10@gmail.com 
+
+### License
+    * This project is licensed by [MIT License].
+    Copyright (c) 2019                  
---
+++ js/scripts.js
@@ -0,0 +1,106 @@
+//For toggling on WHAT WE DO
+$(document).ready(function() {
+    $(".onClick1").click(function() {
+        $("#kash1").toggle();
+        $("#click1").toggle();
+    });
+    $(".onClick2").click(function() {
+        $("#kash2").toggle();
+        $("#click2").toggle();
+    });
+    $(".onClick3").click(function() {
+        $("#kash3").toggle();
+        $("#click3").toggle();
+    });
+});
+
+// For hover effect on PORTFOLIO
+$(document).ready(function()  {
+    $("#work1").mouseover(function()  {
+        $("pWork1").show();
+    });
+    $("#work1").mouseleave(function()  {
+        $("#pWork1").hide();
+    });
+
+    $('#work2').mouseover(function() {
+        $('#pWork2').show();
+    });
+    $('#work2').mouseleave(function() {
+        $('#pWork2').hide();
+    });
+
+    $('#work3').mouseover(function() {
+        $('#pWork3').show();
+    });
+    $('#work3').mouseleave(function() {
+        $('#pWork3').hide();
+    });
+
+    $('#work4').mouseover(function() {
+        $('#pWork4').show();
+    });
+    $('#work4').mouseleave(function() {
+        $('#pWork4').hide();
+    });
+
+    $('#work5').mouseover(function() {
+        $('#pWork5').show();
+    });
+    $('#work5').mouseleave(function() {
+        $('#pWork5').hide();
+    });
+
+    $('#work6').mouseover(function() {
+        $('#pWork6').show();
+    });
+    $('#work6').mouseleave(function() {
+        $('#pWork6').hide();
+    });
+
+    $('#work7').mouseover(function() {
+        $('#pWork7').show();
+    });
+    $('#work7').mouseleave(function() {
+        $('#pWork7').hide();
+    });
+
+    $('#work8').mouseover(function() {
+        $('#pWork8').show();
+    });
+    $('#work8').mouseleave(function() {
+        $('#pWork8').hide();
+    });
+
+});
+
+var btn = document.getElementById("myBtn");
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function(event) {
+    // Validate form for entry
+    var x = document.forms["myForm"]["contact-name"].value;
+    if (x == "") {
+        alert("Name not entered");
+        return false;
+    }
+    var y = document.forms["myForm"]["contact-email"].value;
+    if (y == "") {
+        alert("Email not entered");
+        return false;
+    }
+    var z = document.forms["myForm"]["contact-message"].value;
+    if (z == "") {
+        alert("You have no message to send");
+        return false;
+    }
+    var person1Input = $("input#contact-name").val();
+    $("span#outputName").append(person1Input);
+
+    $('.toast').toast('show');
+    event.preventDefault();
+}
+// MDB Lightbox Init
+$(function  ()  {
+    $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+});
